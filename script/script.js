c51fef3c1680b31aad3ef4eb80d023031d881d39
joyal rebera <joyalrebera40@gmail.com>
2022-02-03 22:38:22 +0530

    initial commit

---
+++ script/script.js
@@ -0,0 +1,130 @@
+// lightgalery js
+
+lightGallery(document.querySelector(".gallery .gallery-container"));
+
+// wow.js
+new WOW().init();
+
+// swiper js
+var swiper = new Swiper(".home-slider", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 7500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  loop: true,
+});
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  centeredSlides: true,
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
+  loop: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  }
+});
+
+
+
+
+
+
+
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+  //for toggling the menubar on small screen
+}
+
+//scrollspy
+let section = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header .navbar a");
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  themeToggler.classList.remove("active");
+
+  section.forEach(sec => {
+
+    let top = window.scrollY;
+    let height = sec.offsetHeight;
+    let offset = sec.offsetTop - 150;
+    let id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+
+      navLinks.forEach(links => {
+        links.classList.remove("active");
+        document.querySelector("header .navbar a[href*=" + id + "]").classList.add("active");
+      })
+    }
+
+  })
+}
+
+//loadmore
+let loadMore=document.querySelector(".load-more");
+let currentItem = 4;
+
+loadMore.onclick=()=>{
+  let boxes=[...document.querySelectorAll(".menu .box")];
+
+  for(let i=currentItem;i<currentItem + 4;i++){
+    boxes[i].style.display="inline-block";
+  }
+  currentItem += 4;
+
+  if (currentItem >= boxes.length) {
+    loadMore.style.display="none";
+  }
+}
+
+
+
+document.querySelector("#search-icon").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+}
+
+document.querySelector("#close").onclick = () => {
+  document.querySelector("#search-form").classList.toggle("active");
+}
+
+let themeToggler=document.querySelector(".theme-toggler");
+let toggleBtn=document.querySelector(".toggle-btn");
+
+toggleBtn.onclick=()=>{
+  themeToggler.classList.toggle("active");
+}
+
+
+document.querySelectorAll(".theme-toggler .theme-btn").forEach(btn =>{
+  btn.onclick = () => {
+    console.log(btn);
+    let color = btn.style.backgroundColor;
+    document.querySelector(":root").style.setProperty("--green", color);
+  }
+});
